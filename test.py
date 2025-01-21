8a9f1175da90d63319a37f9e2f025b2bb1dc6abe
hayabusa2-1998ky26 <d23021ei@shibaurafzk.com>
2025-01-21 19:35:33 +0900

    2024/01/21 first commit

---
+++ test.py
@@ -0,0 +1,29 @@
+# import
+import math
+
+# parameters
+min_speed = 20
+max_speed = 100
+speed = min_speed
+accelerater = 10
+
+speeds = []
+
+# accel
+for i in range(int(360 / accelerater)):
+    acceleration = (1 - math.cos(math.radians(i * accelerater))) * (max_speed - min_speed) / (360 / accelerater)
+    speed += acceleration
+    speeds.append(speed)
+
+# copy accel
+speeds2 = speeds.copy()
+
+# max speed
+speeds += list(int(100 - (360 / accelerater * 2)) * [max_speed])
+
+# decel
+speeds += list(reversed(speeds2))
+
+# screen
+for i in speeds:
+    print(int(i) * "-")
